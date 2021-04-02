6e6d8d99a6d31a7cd7766a80ca590b29d2e0e2e1
TheCatWest <catwestover@outlook.com>
2021-04-02 11:47:57 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// const animalArray = ['dog', 'cat', 'pig'];
+
+// animalArray.push('cow');
+// const personObj = {
+//     name: 'Lernantino',
+//     age: 99
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+      console.log(profileDataArr[i]);
+    }
+  };
