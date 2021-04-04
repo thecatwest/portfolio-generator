72301d256b48b936615459f02dda044020af29aa
TheCatWest <catwestover@outlook.com>
2021-04-04 01:35:23 -0700

    modularize the code

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += i) {
-      console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-templates.js');
 
-    console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-  };
-  
-  printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error (err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
