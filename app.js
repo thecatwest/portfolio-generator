78c2496d1e698cba92cee8c12b092e01352dee15
TheCatWest <catwestover@outlook.com>
2021-04-06 13:40:18 -0700

    modify HTML template to handle more data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
-// const { profile } = require("node:console");
+const generatePage = require("./src/page-templates");
 
 const promptUser = () => {
   return inquirer
@@ -49,7 +50,7 @@ const promptUser = () => {
         }
       },
     ]);
-};
+}; 
 
 const promptProject = portfolioData => {
   console.log(`
@@ -142,16 +143,13 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw err;
+
+      console.log("Portfolio complete! Check out index.html to see the output!");
+    });
   });
 
-// const fs = require("fs");
-// const generatePage = require('./src/page-templates.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
